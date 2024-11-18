f5ffaba8bf22998037341f25c26347a148ede144
Kolezhniuk <D.Kolezhiniyk@gmail.com>
2024-11-18 11:49:42 +0400

    Update config

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -2,6 +2,11 @@ import path from "node:path";
 import { fileURLToPath } from "node:url";
 import js from "@eslint/js";
 import { FlatCompat } from "@eslint/eslintrc";
+import typescriptEslint from "@typescript-eslint/eslint-plugin";
+import prettier from "eslint-plugin-prettier";
+import cspellEslintPlugin from "@cspell/eslint-plugin";
+import tsParser from "@typescript-eslint/parser";
+
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
